f76a29d78d79be2a661f2c03b7f139448e05ee27
Andrewisnew <zakomornii1998@gmail.com>
2018-05-17 16:58:30 +0300

    added exception handlers and showing inner state

--- src/main/java/servlets/SimpleServlet.java
+++ src/main/java/servlets/SimpleServlet.java
@@ -1,45 +1,76 @@
 package servlets;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SimpleServlet extends HttpServlet {
+    private static final String INFO = "INFO";
+    private List<String> stringList = new ArrayList<String>();
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        stringList.add(request.getParameter("value"));
         System.out.println("POST");
         HttpSession session = request.getSession();
-        session.setAttribute("log", "POST");
-        super.doPost(request, response);
+        session.setAttribute(INFO, "Added element:" + request.getParameter("value"));
+        setStringListSessionParam(request);
     }
 
     @Override
-    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPut(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("PUT");
-        HttpSession session = req.getSession();
-        session.setAttribute("log", "PUT");
-        super.doPut(req, resp);
+        HttpSession session = request.getSession();
+        try {
+            String prevValue = stringList.set(0, request.getParameter("value"));
+            session.setAttribute(INFO, "Change first element (" + prevValue
+                    + " => " + request.getParameter("value") + ")");
+        }catch (IndexOutOfBoundsException e) {
+            session.setAttribute(INFO, "List is empty");
+        }
+        setStringListSessionParam(request);
     }
 
     @Override
-    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doDelete(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("DELETE");
-        HttpSession session = req.getSession();
-        session.setAttribute("log", "DELETE");
-        super.doDelete(req, resp);
+        HttpSession session = request.getSession();
+        try {
+            String removedValue = stringList.remove(Integer.parseInt(request.getParameter("value")));
+            session.setAttribute(INFO, "Removed value: " + removedValue);
+        }catch (IndexOutOfBoundsException e) {
+            session.setAttribute(INFO, "There are only " + stringList.size() + " elements in list");
+        }catch (NumberFormatException e) {
+            session.setAttribute(INFO, "Expected integer, but was: " + request.getParameter("value"));
+        }
+        setStringListSessionParam(request);
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-//        response.setContentType("text/html");
         System.out.println("GET");
         HttpSession session = request.getSession();
-        session.setAttribute("log", "GGGET");
+        try {
+            String value = stringList.get(Integer.parseInt(request.getParameter("value")));
+            session.setAttribute(INFO, value);
+        }catch (IndexOutOfBoundsException e) {
+            session.setAttribute(INFO, "There are only " + stringList.size() + " elements in list");
+        }catch (NumberFormatException e) {
+            session.setAttribute(INFO, "Expected integer, but was: " + request.getParameter("value"));
+        }
+        setStringListSessionParam(request);
+    }
+
+    private void setStringListSessionParam(HttpServletRequest req){
+        HttpSession session = req.getSession();
+        if(session.getAttribute("state") == null) {
+            session.setAttribute("state", stringList);
+        }
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,4 +1,5 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%--<html>--%>
 <%--<head>--%>
 <%--<title>gaga</title>--%>
@@ -14,7 +15,9 @@
     <title>Simple web app</title>
     <script type="text/javascript">
         function doIt(method) {
-            var url = "simpleServlet";
+
+            var url = "simpleServlet?value=" +  document.getElementById(method.toLowerCase()).value;
+
             if (window.XMLHttpRequest) {
                 request = new XMLHttpRequest();
             } else if (window.ActiveXObject) {
@@ -40,26 +43,34 @@
 <body>
 
 <p>
-    <label>GET</label>
+    <label>Get element by index: </label>
     <input type="text" name="username" id="get">
     <button type="button" onclick="doIt('GET')">GET</button>
 </p>
 <p>
-    <label>POST</label>
+    <label>Add new element: </label>
     <input type="text" name="username" id="post">
     <button type="button" onclick="doIt('POST')">POST</button>
 </p>
 <p>
-    <label>PUT</label>
+    <label>Update first element: </label>
     <input type="text" name="username" id="put">
     <button type="button" onclick="doIt('PUT')">PUT</button>
 </p>
 <p>
-    <label>DELETE</label>
+    <label>Remove element by index: </label>
     <input type="text" name="username" id="delete">
     <button type="button" onclick="doIt('DELETE')">DELETE</button>
 </p>
-
-<p>${sessionScope.log}</p>
+<h4>RESULT: </h4>
+<p>${sessionScope.INFO}</p>
+<h4>Inner state:</h4>
+<c:if test="${not empty sessionScope.state}">
+    <c:set var="counter" value="${0}" />
+        <c:forEach items="${sessionScope.state}" var="item">
+            ${counter} : ${item} <br/>
+            <c:set var="counter" value="${counter+1}" />
+        </c:forEach>
+</c:if>
 </body>
 </html>
